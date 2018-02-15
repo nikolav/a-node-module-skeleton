4c8583d06f0db878f4aa698b7e1dac2f728339a7
nikola.v <vukovicnikola2014@gmail.com>
2018-02-15 05:34:54 +0100

    ...

--- 01.js
+++ 01.js
@@ -1,15 +1,72 @@
 
-var express = require('express');
-var app     = express();
+// var express = require('express');
+// var app     = express();
+// app.set('port', process.env.PORT || 3000);
+// app.get('/', function (req, res) {
+//   res.send('EXPRESS WORKS = true;');
+// });
+// app.listen(app.get('port'), function () {
+//   console.log('express started; press Ctrl-C to terminate');
+// });
 
-app.set('port', process.env.PORT || 3000);
-app.get('/', function (req, res) {
-  res.send('EXPRESS WORKS;');
-});
+;(function (exports, g, none) {
+  
+  // privates
+  var cache = {};
+  var pCtor;
+  var bind   = Function.prototype.call.bind(Function.prototype.bind);
+  var hasown = bind(Function.prototype.call, Object.prototype.hasOwnProperty);
+  
+  
+  // module api port
+  function Ctor () {
+    
+  }
+  
+  
+  // export for node
+  exports["ctor"] = Ctor;
 
-app.listen(app.get('port'), function () {
-  console.log('express started; press Ctrl-C to terminate');
-});
+
+  pCtor = Ctor.prototype;
+  
+  // public
+  paste(pCtor, {
+    "a1": f1
+  , "a2": f2
+  , "aN": fN
+  });
+  
+  // static
+  paste(Ctor, {
+
+  });
+  
+  
+  // api
+
+  function f1 () {
+
+  }
+
+  function f2 () {
+
+  }
+
+  function fN () {
+
+  }
+  
+  // helpers
+  function paste (node, props) {
+    for (var name in props) {
+      if (hasown(props, name)) {
+        node[name] = props[name];
+      }
+    }
+  }
+
+})(exports, this, null);
+
 
 // eof
-
---
+++ README.md
@@ -0,0 +1,1 @@
+an idea for wrapping a node module
